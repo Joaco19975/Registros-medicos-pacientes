521d78897238b6afc95fa9dd795c0da39c21dc85
Joaquin Paradela <joaquin.paradela28@gmail.com>
2023-02-07 21:23:55 -0300

    web

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HospitalController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [HospitalController::class, 'register']);
+
