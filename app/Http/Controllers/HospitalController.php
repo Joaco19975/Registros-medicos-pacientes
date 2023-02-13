06c62437cc34041eb52e8825aba7988dae78fcdf
Joaquin Paradela <joaquin.paradela28@gmail.com>
2023-02-13 17:10:19 -0300

    arreglo login

--- app/Http/Controllers/HospitalController.php
+++ app/Http/Controllers/HospitalController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Patient;
+use Illuminate\Support\Facades\Auth;
 
 class HospitalController extends Controller
 {
